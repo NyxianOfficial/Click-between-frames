33c61978fa7695887b53a7524996eda51a11ccf0
NyxianOfficial <angeldboadaro@gmail.com>
2025-04-06 18:58:29 +0200

    Update 1.0.2

--- src/includes.hpp
+++ src/includes.hpp
@@ -3,14 +3,16 @@
 #include <algorithm>
 #include <queue>
 #include <mutex>
+#include <array>
 #include <unordered_set>
 #include <atomic>
-#include <cstdint> // Para tipos estándar como uint64_t lo use para cambiar codigo de windows a android
+#include <chrono>
+
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
-enum class GameAction : int {
+enum GameAction : int {
     p1Jump = 0,
     p1Left = 1,
     p1Right = 2,
@@ -19,24 +21,23 @@ enum class GameAction : int {
     p2Right = 5
 };
 
-enum class InputState : int {
-    Press = 0,
-    Release = 1
+enum State : bool {
+    Release = 0,
+    Press = 1
+};
+
+struct __attribute__((packed)) LinuxInputEvent {
+    std::chrono::steady_clock::time_point time;
+    uint16_t type;
+    uint16_t code;
+    int value;
 };
 
 struct InputEvent {
-    uint32_t time;
-    InputState inputState;
+    std::chrono::steady_clock::time_point time;
+    PlayerButton inputType;
+    bool inputState;
     bool isPlayer1;
-
-    InputEvent& operator=(const InputEvent& other) {
-        if (this != &other) {
-            time = other.time;
-            inputState = other.inputState;
-            isPlayer1 = other.isPlayer1;
-        }
-        return *this;
-    }
 };
 
 struct Step {
@@ -45,16 +46,20 @@ struct Step {
     bool endStep;
 };
 
-// Variables globales
+// Declaraciones de variables usando extern 
 extern std::queue<InputEvent> inputQueue;
-extern std::queue<InputEvent> inputQueueCopy;
-extern std::unordered_set<uint16_t> heldInputs; 
+extern std::queue<InputEvent> inputQueueCopy; // Asegúrate de que esté aquí
+extern std::array<std::unordered_set<size_t>, 6> inputBinds;
+extern std::unordered_set<uint16_t> heldInputs;
+
 extern std::mutex inputQueueLock;
+extern std::mutex keybindsLock;
+
 extern std::atomic<bool> enableRightClick;
-extern std::atomic<bool> softToggle;
+extern std::atomic<bool> softToggle; 
+
 extern bool threadPriority;
 
 constexpr size_t BUFFER_SIZE = 20;
-
 
 void inputThread();
